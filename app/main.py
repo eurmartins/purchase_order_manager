404f4c2e93094771ee099c0292f884db80b6cd18
Eurmartins <victormartinssantos.work@gmail.com>
2025-09-07 12:42:19 -0300

    feat: set gitignore in the project

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from app.api import router
 from app.db.base import Base
 from app.db.session import engine
 
+#Cria as tabelas no banco de dados
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
