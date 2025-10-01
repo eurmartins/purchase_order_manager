4bf0d0ff8eb1c426f0412be6bab7d2b3fd4f4ef5
Eurmartins <victormartinssantos.work@gmail.com>
2025-09-30 23:04:09 -0300

    refactor: include role and status_order routers in API router

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
-from .endpoints import user, auth, purchase_orders
+from .endpoints import user, auth, purchase_orders, role, status_order
 
 api_router = APIRouter()
+
+api_router.include_router(role.router, prefix="/role", tags=["role"])
+api_router.include_router(status_order.router, prefix="/status_order", tags=["status_order"])
 api_router.include_router(user.router, prefix="/users", tags=["users"])
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
-
-api_router.include_router(purchase_orders.router,  prefix="/purchase_orders", tags=["purchase_orders"])
+api_router.include_router(purchase_orders.router, prefix="/purchase_orders", tags=["purchase_orders"])
